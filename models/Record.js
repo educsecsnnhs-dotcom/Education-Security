fd30f0de2d2d49e33c36ae8fa47b614f3194a7e0
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-08 15:36:06 +0800

    Update Record.js

--- models/Record.js
+++ models/Record.js
@@ -1,1 +1,18 @@
+// models/Record.js
+const mongoose = require('mongoose');
 
+
+const recordSchema = new mongoose.Schema(
+{
+class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
+row: { type: Number, required: true },
+data: { type: Object, required: true } // arbitrary key/value from sheet row
+},
+{ timestamps: true }
+);
+
+
+recordSchema.index({ class: 1, row: 1 }, { unique: true });
+
+
+module.exports = mongoose.model('Record', recordSchema);
