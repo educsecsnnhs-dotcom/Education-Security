1274ccd1d04dfd33248f850f273bb4c0143212e8
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-08 15:35:15 +0800

    Update Class.js

--- models/Class.js
+++ models/Class.js
@@ -1,1 +1,17 @@
+// models/Class.js
+const mongoose = require('mongoose');
 
+
+const classSchema = new mongoose.Schema(
+{
+name: { type: String, required: true }, // e.g., "Math 10 - Section A"
+subjectCode: { type: String }, // optional code
+description: { type: String },
+sheetId: { type: String, required: true }, // Google Sheet ID
+moderators: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+},
+{ timestamps: true }
+);
+
+
+module.exports = mongoose.model('Class', classSchema);
