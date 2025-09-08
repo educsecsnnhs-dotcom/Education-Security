b2dabbfc6ecd4a80843e87923f29a980bd246be0
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-08 15:33:42 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
+// config/db.js
+const mongoose = require('mongoose');
 
+
+async function connectDB() {
+const uri = process.env.MONGODB_URI;
+if (!uri) throw new Error('MONGODB_URI is missing');
+await mongoose.connect(uri, { dbName: process.env.MONGODB_DB || undefined });
+console.log('✅ MongoDB connected');
+}
+
+
+module.exports = connectDB;
