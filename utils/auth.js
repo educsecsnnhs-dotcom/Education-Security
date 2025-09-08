2a47eb54e47a50ce98e665082fba0edb2d107d54
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-08 15:33:19 +0800

    Update auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,19 @@
+// utils/auth.js
+const jwt = require('jsonwebtoken');
 
+
+function signToken(user) {
+return jwt.sign(
+{ id: user._id, username: user.username, roles: user.roles },
+process.env.JWT_SECRET,
+{ expiresIn: '7d' }
+);
+}
+
+
+function verifyToken(token) {
+return jwt.verify(token, process.env.JWT_SECRET);
+}
+
+
+module.exports = { signToken, verifyToken };
