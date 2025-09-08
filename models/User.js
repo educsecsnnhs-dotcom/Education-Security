758a51b3341668897a2bf7d9859ab929f9f40def
educsecsnnhs-dotcom <educsecsnnhs@gmail.com>
2025-09-08 15:34:47 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,20 @@
+// models/User.js
+const mongoose = require('mongoose');
 
+
+const userSchema = new mongoose.Schema(
+{
+username: { type: String, unique: true, required: true, trim: true },
+password: { type: String, required: true }, // Caesar-encrypted value
+roles: {
+type: [String],
+default: ['User'],
+enum: ['User', 'Student', 'Moderator', 'SSG', 'Registrar', 'Admin', 'Super Admin']
+},
+meta: { type: mongoose.Schema.Types.Mixed }
+},
+{ timestamps: true }
+);
+
+
+module.exports = mongoose.model('User', userSchema);
